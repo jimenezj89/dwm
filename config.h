b92640af7c462971e5f2d17813b99c5a0c6f8173
Javier Jiménez <javier.jimenez.masip@gmail.com>
2020-11-23 20:04:04 +0100

    Rofi as application launcher

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 5;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -10,8 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGL Nerd Font Mono:size=10" };
-static const char dmenufont[]       = "MesloLGL Nerd Font Mono:size=10";
+static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -60,13 +60,16 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char rofimon[2] = "0";
+static const char *roficmd[] = { "rofi", "-show", "drun"};
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *volumedown[] = { "/usr/bin/pulseaudio-ctl", "down", NULL };
+static const char *volumemute[] = { "/usr/bin/pulseaudio-ctl", "mute", NULL };
+static const char *volumeup[] = { "/usr/bin/pulseaudio-ctl", "up", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -78,7 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	/*{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },*/
-	{ MODKEY,             XK_w,      killclient,     {0} },
+	{ MODKEY,                       XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -103,6 +106,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0,                            XF86XK_AudioLowerVolume,    spawn,  {.v = volumedown } },
+    { 0,                            XF86XK_AudioMute,           spawn,  {.v = volumemute } },
+    { 0,                            XF86XK_AudioRaiseVolume,    spawn,  {.v = volumeup } },
 };
 
 /* button definitions */
